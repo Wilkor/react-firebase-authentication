93f57a091e89809692d292c5a2aed5522c045cd0
wilkor <wilkorguitarrista@msn.com>
2020-07-12 19:46:05 -0300

    ajuste no layout

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -20,18 +20,17 @@ const Navigation = () => (
 );
 
 const NavigationAuth = ({ authUser }) => (
+
   <>
-  <nav className="navbar navbar-dark bg-dark navbar-expand-sm">
-  <a className="navbar-brand" href="#">
-  <img src={authUser.providerData[0].photoURL} width="40" height="40" className="rounded-circle"/>
- 
-  </a>
-  
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-list-4" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
+  <nav className="navbar navbar-expand-lg navbar-light bg-light navbar navbar-dark bg-dark">
+  <Link className="navbar-brand" to="#"><img src={authUser.providerData[0].photoURL}  className="rounded-circle" alt="" width="50"/></Link>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
-  <div className="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul className="navbar-nav">
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+
+  <ul className="navbar-nav">
       <li className="nav-item active">
       <Link to="/change" className="nav-link" href="#">Feed de Troca</Link>
         
@@ -54,14 +53,56 @@ const NavigationAuth = ({ authUser }) => (
       </li> */}
    
       <li className="nav-item">
-      <SignOutButton/>
-      </li>
-    </ul>
+       <SignOutButton/>
+       </li>
+     </ul>
+
   </div>
-
 </nav>
+</>
 
-  </>
+//   <>
+//   <nav className="navbar navbar-dark bg-dark navbar-expand-sm">
+//   <a className="navbar-brand" href="#">
+//   <img src={authUser.providerData[0].photoURL} width="40" height="40" className="rounded-circle"/>
+ 
+//   </a>
+  
+//   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-list-4" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
+//     <span className="navbar-toggler-icon"></span>
+//   </button>
+//   <div className="collapse navbar-collapse" id="navbarNavDropdown">
+//     <ul className="navbar-nav">
+//       <li className="nav-item active">
+//       <Link to="/change" className="nav-link" href="#">Feed de Troca</Link>
+        
+//       </li>
+//       <li class="nav-item dropdown">
+//         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+//           Produto
+//         </a>
+//         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+//           <Link to="/product" class="dropdown-item" href="#">Incluir Produto</Link>
+//           <Link to="/myproducts" class="dropdown-item" href="#">Meus Produtos</Link>
+//         </div>
+//       </li>
+//       {/* <li className="nav-item">
+//         <Link to="/notification" className="nav-link" href="#">Notificações</Link>
+//       </li>
+//       <li className="nav-item">
+//       <Link to="/profile" className="nav-link" href="#">Perfil</Link>
+      
+//       </li> */}
+   
+//       <li className="nav-item">
+//       <SignOutButton/>
+//       </li>
+//     </ul>
+//   </div>
+
+// </nav>
+
+//   </>
 );
 
 const NavigationNonAuth = () => (
